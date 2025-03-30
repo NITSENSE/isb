f75569a8411f5323b39e18f6df0176472f9f284c
NITSENSE KO <volodia.kostsoff@yandex.ru>
2025-03-30 23:43:05 +0400

    Added descriptions to the enumerated classes

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,14 +4,30 @@ from task_1.text_encryptor import arg_parser_task_1, encript, decrypt
 from task_2.frequency_analysis import arg_parser_task_2, freguency_analis, match_alphabets_by_frequency, decrypt_text
 
 
-class Goal(Enum):
-    FREQUENCY_ANALYSIS = 1
-    LOCATION_SELECTION = 2
-
-
 class Task(Enum):
+    """
+    Выбор задачи.
+
+    Attributes:
+        TASK_1 (int): Задание 1.
+        TASK_2 (int): Задание 2.
+    """
     TASK_1 = 1
     TASK_2 = 2
+
+
+class Goal(Enum):
+    """
+    Выбор порядка выполнения второго задания.
+
+    Attributes:
+        FREQUENCY_ANALYSIS (int): Выполнить частотный анализ, 
+            получить возможный вариант расшифровки текста.
+        LOCATION_SELECTION (int): Проверка гипотиз,
+            когда частотный анализ уже был произведен.
+    """
+    FREQUENCY_ANALYSIS = 1
+    LOCATION_SELECTION = 2
 
 
 def main():
