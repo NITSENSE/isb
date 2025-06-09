8f3a6a405b0d5da859d3d132ad3d8f1733b13e5e
NITSENSE KO <volodia.kostsoff@yandex.ru>
2025-06-09 08:14:03 +0400

    Encrypting the symmetric "IDEA" key with an RSA public key

--- lab_3/hybrid_crypto/rsa_operations.py
+++ lab_3/hybrid_crypto/rsa_operations.py
@@ -1,6 +1,9 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from work_file import write_file
+from cryptography.hazmat.primitives.asymmetric import padding as rsa_padding
+from cryptography.hazmat.primitives import hashes
+from idea_operations import generate_idea_key
 
 
 RSA_PUBLIC_EXPONENT = 65537
@@ -49,7 +52,34 @@ def serialize_rsa_keys(public_key: rsa.RSAPublicKey, private_key: rsa.RSAPrivate
     print(f"The RSA private key is stored in: {private_key_path}")
 
 
+def encrypt_symmetric_key_rsa(symmetric_key: bytes, public_key: rsa.RSAPublicKey,
+                              encrypted_symmetric_key_path: str) -> None:
+    """
+    Шифрует симметричный ключ с помощью публичного ключа RSA (OAEP) и сохраняет в файл.
+    Параметры:
+        symmetric_key (bytes): Симметричный ключ (ключ IDEA).
+        public_key (RSAPublicKey): Публичный ключ RSA.
+        encrypted_symmetric_key_path (str): Путь для сохранения зашифрованного симметричного ключа.
+    """
+    encrypted_symmetric_key = public_key.encrypt(
+        symmetric_key,
+        rsa_padding.OAEP(
+            mgf=rsa_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    write_file(encrypted_symmetric_key_path, encrypted_symmetric_key)
+    print(f"The symmetric key is encrypted with RSA and stored in: {encrypted_symmetric_key_path}")
+
+
 if __name__ == '__main__':
+
+    from idea_operations import generate_idea_key 
+    
+    idea_s_key = generate_idea_key()
     priv_key_obj, pub_key_obj = generate_rsa_keys()
-    serialize_rsa_keys(pub_key_obj, priv_key_obj, 'lab_3/keys/public.pem', 'lab_3/keys/private.pem')
-    print("Ключи RSA сгенерированы и сериализованы")
+
+    serialize_rsa_keys(pub_key_obj, priv_key_obj, 'lab_3/keys/public.pem', 'lab_3/keys/public.pem')
+    encrypt_symmetric_key_rsa(idea_s_key, pub_key_obj, 'lab_3/keys/encrypted_sym_key.bin')
+    print("Полный цикл генерации и шифрования симметричного ключа завершен для примера.")
